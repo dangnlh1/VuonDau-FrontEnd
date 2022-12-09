f2b90f781883959d504ea822f2196b20fd116cae
minhhungle <haless132@gmail.com>
2022-12-09 17:09:43 +0700

    update banner

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -2,10 +2,7 @@ import { Autoplay, Navigation, Pagination } from 'swiper'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import { Swiper, SwiperSlide } from 'swiper/react'
-
 import { alpha, Box, Stack } from '@mui/material'
-import banner1 from '@/assets/images/banner1.png'
-import banner2 from '@/assets/images/banner2.jpg'
 import { BannerPayload } from '@/models/banner'
 
 export interface BannerProps {
@@ -18,7 +15,7 @@ export function Banner({ bannerList }: BannerProps) {
   }
 
   return (
-    <Box boxShadow={2} sx={{ borderRadius: '4px', overflow: 'hidden', height: 400 }}>
+    <Box boxShadow={2} sx={{ borderRadius: '4px', overflow: 'hidden' }}>
       <Swiper
         pagination={pagination}
         navigation={true}
@@ -33,24 +30,32 @@ export function Banner({ bannerList }: BannerProps) {
           bannerList.length > 0 &&
           bannerList.map((item, idx) => (
             <SwiperSlide key={idx}>
-              <Box
-                boxShadow={3}
-                sx={{
-                  height: 400,
-                  width: '100%',
-
-                  backgroundImage: `url(${item.linkUrl})`,
-                  backgroundRepeat: 'no-repeat',
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-                }}
-              >
-                <Stack
+              <Box sx={{ position: 'relative', height: 0, paddingTop: '45%' }}>
+                <Box
+                  boxShadow={3}
                   sx={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+
                     height: '100%',
-                    bgcolor: (theme) => alpha(theme.palette.common.black, 0.1),
+                    width: '100%',
+
+                    backgroundImage: `url(${item.linkUrl})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
                   }}
-                />
+                >
+                  <Stack
+                    sx={{
+                      height: '100%',
+                      bgcolor: (theme) => alpha(theme.palette.common.black, 0.1),
+                    }}
+                  />
+                </Box>
               </Box>
             </SwiperSlide>
           ))}
