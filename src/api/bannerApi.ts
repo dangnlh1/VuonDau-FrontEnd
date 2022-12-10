985601ab301b9c7a61bc6407e2a5e1ad6d7e17c2
minhhungle <haless132@gmail.com>
2022-12-11 00:57:17 +0700

    fix

--- src/api/bannerApi.ts
+++ src/api/bannerApi.ts
@@ -1,13 +1,8 @@
 import { BannerPayload } from '@/models/banner'
 import axiosClient from './axiosClient'
 
-interface BannerResponse {
-  status: string
-  data: BannerPayload[]
-}
-
 export const bannerApi = {
-  getAll(): Promise<BannerResponse> {
+  getAll(): Promise<BannerPayload[]> {
     return axiosClient.get('/pano/panos')
   },
 }
